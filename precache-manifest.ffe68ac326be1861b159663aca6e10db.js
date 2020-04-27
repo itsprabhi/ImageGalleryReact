42ad4367e23c2982ca637b55e6d80a742ffe6742
itsprabhi <iamprabhie@gmail.com>
2020-04-27 19:44:22 -0400

    Updates

---
+++ precache-manifest.ffe68ac326be1861b159663aca6e10db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f281f79c775fb288ec5c77184ab8229a",
+    "url": "/ImageGalleryReact/index.html"
+  },
+  {
+    "revision": "d3b7397f4070651ca441",
+    "url": "/ImageGalleryReact/static/css/main.f39962af.chunk.css"
+  },
+  {
+    "revision": "db1ac1a9861b8c037707",
+    "url": "/ImageGalleryReact/static/js/2.a0f69778.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageGalleryReact/static/js/2.a0f69778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b7397f4070651ca441",
+    "url": "/ImageGalleryReact/static/js/main.2e6f3ab1.chunk.js"
+  },
+  {
+    "revision": "6db784c2be979b163492",
+    "url": "/ImageGalleryReact/static/js/runtime-main.3750eaa1.js"
+  }
+]);
